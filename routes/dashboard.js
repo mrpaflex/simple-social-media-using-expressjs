74c6a41067425a30de3f27e67e3502cfb9ac784a
mrpaflex <ukosaviour21@gmail.com>
2023-10-01 06:03:34 +0100

    change on post route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {ensureAuth} = require('../middleware/authGuard')
-const dashBController = require('../controllers/dashboard')
-// const postController = require('../controllers/postF')
-// const upload = require("../middleware/multer");
+const {ensureAuth} = require('../middleware/authGuard');
+const dashBController = require('../controllers/dashboard');
 
 
 router.get('/', ensureAuth, dashBController.gotoDashboard)
-// router.get('/post', ensureAuth, postController.gotopost )
-// router.get('/myPost', ensureAuth, postController.myPosts )
-
-// router.post('/createPost', ensureAuth, upload.single("file"), postController.MakePost )
 
 module.exports = router;
