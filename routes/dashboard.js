351d92111ecbfd835ed40ce8fade3e233820f546
mrpaflex <ukosaviour21@gmail.com>
2023-10-01 06:27:38 +0100

    delete and post changes made

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 const {ensureAuth} = require('../middleware/authGuard');
 const dashBController = require('../controllers/dashboard');
+const postController = require('../controllers/postF')
+const upload = require("../middleware/multer");
 
 
 router.get('/', ensureAuth, dashBController.gotoDashboard)
+router.get('/myPost', ensureAuth, postController.myPosts )
+//uploading
+router.post('/createPost', ensureAuth, upload.single("file"), postController.MakePost )
+//going to posting route
+router.get('/post', ensureAuth, postController.gotopost )
+
+//here is for delete
+router.delete('/deletePost/:id', ensureAuth,  postController.deletePost)
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,6 @@ const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 const {ensureAuth} = require('../middleware/authGuard')
 const postController = require('../controllers/postF')
-const upload = require("../middleware/multer");
 
 router.get('/', homeController.gethome)
 router.get('/login', homeController.getlogin)
@@ -13,14 +12,11 @@ router.get('/signup', homeController.getSignup)
 router.post('/signup', authController.signup)
 router.get('/logout', authController.logout)
 
-//making posting route
-router.get('/post', ensureAuth, postController.gotopost )
-router.get('/myPost', ensureAuth, postController.myPosts )
-router.post('/createPost', ensureAuth, upload.single("file"), postController.MakePost )
+
+
 
 //here is for like
-router.put('/likePost/:id', ensureAuth,  postController.likePosts)
-router.delete('/deletePost/:id', ensureAuth,  postController.deletePost)
+router.put('/likePost/:id', ensureAuth,  postController.likePosts);
 
 
 
