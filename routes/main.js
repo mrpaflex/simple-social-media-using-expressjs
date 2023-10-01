f75be07cadde0099538bdf0e2d5b991108af01ff
mrpaflex <ukosaviour21@gmail.com>
2023-10-01 07:14:02 +0100

    delete route changed

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 const {ensureAuth} = require('../middleware/authGuard')
 const postController = require('../controllers/postF')
+const upload = require("../middleware/multer");
 
 router.get('/', homeController.gethome)
 router.get('/login', homeController.getlogin)
@@ -12,8 +13,14 @@ router.get('/signup', homeController.getSignup)
 router.post('/signup', authController.signup)
 router.get('/logout', authController.logout)
 
+router.get('/myposts', ensureAuth, postController.myPosts )
+//uploading
+router.post('/createPost', ensureAuth, upload.single("file"), postController.MakePost )
+//going to posting route
+router.get('/post', ensureAuth, postController.gotopost )
 
-
+//here is for delete
+router.delete('/myposts/deletePost/:id', ensureAuth,  postController.deletePost)
 
 //here is for like
 router.put('/likePost/:id', ensureAuth,  postController.likePosts);
